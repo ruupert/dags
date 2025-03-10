d2b23dfa72f11c5c468a8c8ee8a6e57bb4ef2d36
Raul Becker <raul.becker@iki.fi>
2025-03-10 19:41:59 +0200

    exceptions and slackhook

--- yt_dlp_dl.py
+++ yt_dlp_dl.py
@@ -5,6 +5,9 @@ from airflow.operators.bash import BashOperator
 from airflow.models.dag import DAG
 from airflow.models import Variable
 from airflow.utils.dag_parsing_context import get_parsing_context
+from airflow.exceptions import AirflowFailException, AirflowRescheduleException
+from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.operators.empty import EmptyOperator
 
 current_dag_id = get_parsing_context().dag_id
 channels = json.loads(Variable.get('ytchannels'))
@@ -54,11 +57,41 @@ for channel in channels['channels']:
                 'break_on_existing': True,
                 'outtmpl': f'{download_dir}/downloads/%(playlist)s/{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")}_%(title)s.%(ext)s',
             }
+            res = []
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 try:
                     ydl.download([channel['url']])
+                    ydl._format_out()
                 except yt_dlp.utils.ExistingVideoReached:
-                    return 0
-
+                    return 1
+                except yt_dlp.utils.DownloadError as e:
+                    if e.msg.__contains__('This video is available to this channel'):
+                        return 1
+                    else:
+                        return 3
+                except yt_dlp.utils.ExtractorError:
+                    return 2
+                except Exception:
+                    return 2
+                return 0
+        
         ytl = youtube_dl(channel, download_dir)
-        create_dir >> ytl
+        if ytl == 3:            
+            raise AirflowRescheduleException
+        if ytl == 2:
+            raise AirflowFailException
+        
+        if ytl == 0:
+            slack_webhook_operator_text = SlackWebhookOperator(
+            task_id="slack_webhook_send_text",
+            slack_webhook_conn_id="slack_webhook",
+            message=(
+                f"{channel['name']} video(s) downloaded"
+                ),
+            )
+        else:
+            slack_webhook_operator_text = EmptyOperator(
+                task_id="slack_webhook_send_text"
+            )
+
+        create_dir >> ytl >> slack_webhook_operator_text
