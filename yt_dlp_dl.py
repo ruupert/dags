148159853fbec3d160ef1d5dba416c88d6d326ac
Raul Becker <raul.becker@iki.fi>
2025-11-16 23:51:39 +0200

    deno hardcoded

--- yt_dlp_dl.py
+++ yt_dlp_dl.py
@@ -42,7 +42,7 @@ for channel in channels['channels']:
         )
 
         @task.virtualenv(
-            requirements=['yt-dlp', 'requests'],
+            requirements=['yt-dlp[default]', 'requests'],
             task_id="youtube_dl",
             queue="youtube",
             system_site_packages=False
@@ -108,6 +108,9 @@ for channel in channels['channels']:
                 'download_archive': f'{download_dir}/download_archive',
                 'break_on_existing': True,
                 'post_hooks': [dlhook],
+                'js_runtimes': [
+                    'deno:/usr/local/bin/deno'
+                ],
                 'outtmpl': f'{download_dir}/downloads/%(playlist)s/%(title)s-%(id)s.%(ext)s'
             }
             
